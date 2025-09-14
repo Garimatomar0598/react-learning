2bb424adae3ca0af39a6258a4f6c775c31e7011a
Garima Tomar <garimatomar55521@gmail.com>
2025-09-14 23:51:41 +0530

    concepts Understanding

---
+++ react-router/src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+
+function App() {
+  return (
+    <>
+     <Header />
+     <Footer />
+     <Home />
+    </>
+  )
+}
+
+export default App
