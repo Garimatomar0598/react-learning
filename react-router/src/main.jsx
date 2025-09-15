e9243c8210c8ae9227807949c35eb5bc94bc92d0
Garima Tomar <garimatomar55521@gmail.com>
2025-09-15 13:00:31 +0530

    learn react router concepts

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
+import Contact from './Components/Contact/Contact.jsx'
 
 
 const router = createBrowserRouter([
@@ -13,12 +14,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [{
-      path: "/",
+      path: "",
       element: <  Home />
     },{
       path: "about",
       element: <About />
     },{
+      path: "/contact",
+      element: <Contact />
     }]
   }
 ])
