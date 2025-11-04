d205eb6dfae4e5d0319a64ee6ca90e3a6c68a96f
Garima Tomar <garimatomar55521@gmail.com>
2025-11-04 21:04:48 +0530

    form handling understanding

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react'
-import './App.css'
-import UserContext from './Context/UserContext'
-import Login from './Components/Login'
-import Profile from './Components/Profile'
-import Test from './Test'
-import Loggedin from './Loggedin'
+
+import React from 'react'
+import SimpleForm from './SimpleForm'
 
 function App() {
-
-  const [user, setUser] = useState(null);
   return (
- 
-      <UserContext.Provider value={{ user, setUser }}>
-<Login/>
-<Profile/>
-<Test />
-<Loggedin />
-      
-      </UserContext.Provider>
-    
+    <div>
+      < SimpleForm />
+    </div>
   )
 }
 
