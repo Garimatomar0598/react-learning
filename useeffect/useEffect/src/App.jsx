1175ccc77063661e131cbdaafc4e10b8a3708062
Garima Tomar <garimatomar55521@gmail.com>
2025-11-10 20:58:36 +0530

    useEffect Methid in react to render one time component

---
+++ useeffect/useEffect/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState,useEffect } from 'react'
+
+
+function App() {
+const [count,setCount] = useState(0)
+
+useEffect(()=>{
+  console.log("comoponent re-render")
+},[])
+
+//jab jab count ki value change hogi console main print hoga ye message bex of useEffect method 
+
+  return (
+    <>
+<div>
+<h2>Count:{count}</h2>
+<button onClick={()=>setCount(count+1)}>Increment</button>
+
+</div>
+        
+      
+      
+    </>
+  )
+}
+
+export default App
