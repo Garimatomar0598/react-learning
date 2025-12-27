4ff88cd1a89a6d11457713037577bb4bc5498fcf
Garima Tomar <garimatomar55521@gmail.com>
2025-12-27 19:12:53 +0530

    coding journey end here

--- PropsDrilling/props-drillinginreact/src/App.jsx
+++ PropsDrilling/props-drillinginreact/src/App.jsx
@@ -5,14 +5,15 @@ function Parent(){
 const userName="This is Garima Tomar"
 return <Child name ={userName} />
 }
-
+// first parent comopnent banaya fir ek value di isko jis main data save kiya jo grandchild compoment main humko dikhana ho,fir return kar diya child component ko with name property de kar, name ki jagha par hum kuch bhi naam de sakte hai  retrun karvate waqt and varaible name diya hai object main
 function Child({name}){
 return <GrandChild name2 ={name} />
 }
-
+// yaha par reurn ke time par {name} property di hai jo grandchild accept karega jab hum usko denge issi se vo upar ka data access karega
 function GrandChild({name2}){
 return <h3>Hello{name2}</h3>
 }
+// yaha par reurn ke time par {name2} property di hai jo grandchild accept karega jab hum usko denge issi se vo upar ka data access karega
 
 function App() {
   return (
